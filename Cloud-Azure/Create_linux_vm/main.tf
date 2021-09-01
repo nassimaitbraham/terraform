fd719921f97e7ef972dcc48c1f173291ccbfd889
gaitbraham@gmail.com <gaitbraham@gmail.com>
2021-09-01 18:52:05 +0200

    delete auth section from main file

--- Cloud-Azure/Create_linux_vm/main.tf
+++ Cloud-Azure/Create_linux_vm/main.tf
@@ -17,10 +17,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-    subscription_id   = "<azure_subscription_id>"
-    tenant_id         = "<azure_subscription_tenant_id>"
-    client_id         = "<service_principal_appid>"
-    client_secret     = "<service_principal_password>"
 }
 
 # creation du ressource group azure
